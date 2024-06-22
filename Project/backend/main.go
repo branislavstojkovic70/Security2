1e4398e9ad69bb006a777bfb410079c7ee3847b1
branislavstojkovic70 <96660978+branislavstojkovic70@users.noreply.github.com>
2024-06-22 19:07:09 +0200

    Update connection for leveldb

--- Project/backend/main.go
+++ Project/backend/main.go
@@ -2,9 +2,7 @@ package main
 
 import (
     "fmt"
-    "net/http"
     "log"
-    "io/ioutil"
     "os"
     "github.com/syndtr/goleveldb/leveldb"
 )
@@ -12,8 +10,6 @@ import (
 func main() {
     leveldbHost := os.Getenv("LEVELDB_HOST")
     leveldbPort := os.Getenv("LEVELDB_PORT")
-    consulHost := os.Getenv("CONSUL_HOST")
-    consulPort := os.Getenv("CONSUL_PORT")
 
     if leveldbHost == "" {
         leveldbHost = "leveldb-server"
@@ -21,14 +17,7 @@ func main() {
     if leveldbPort == "" {
         leveldbPort = "2012"
     }
-    if consulHost == "" {
-        consulHost = "consul"
-    }
-    if consulPort == "" {
-        consulPort = "8500"
-    }
 
-    // Testiranje konekcije na LevelDB
     dbPath := fmt.Sprintf("http://%s:%s", leveldbHost, leveldbPort)
     fmt.Println("Connecting to LevelDB at", dbPath)
     db, err := leveldb.OpenFile(dbPath, nil)
@@ -38,20 +27,5 @@ func main() {
     defer db.Close()
     fmt.Println("Connected to LevelDB")
 
-    // Testiranje konekcije na Consul
-    consulURL := fmt.Sprintf("http://%s:%s/v1/agent/self", consulHost, consulPort)
-    fmt.Println("Connecting to Consul at", consulURL)
-    resp, err := http.Get(consulURL)
-    if err != nil {
-        log.Fatalf("Failed to connect to Consul: %v", err)
-    }
-    defer resp.Body.Close()
-    body, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        log.Fatalf("Failed to read response from Consul: %v", err)
-    }
-    fmt.Println("Connected to Consul, response:", string(body))
-
-    // Vaš glavni logički kod dolazi ovde
     fmt.Println("Hello, World!")
 }
